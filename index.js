ae40d6a813d8fe8106610d4f4939b8b176199180
Evgeny Klimechenko <Zklimenchenko@gmail.com>
2020-03-09 13:19:41 +0000

    Added dependencies

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('testing')
+const github = require('@actions/github')
+const core = require('@actions/core')
+const myToken = core.getInput('GITHUB_TOKEN')
+const octokit = new github.GitHub(myToken)
+
+
+async function run() {
+  const data = await octokit.pulls.get();
+
+  console.log(data)
+}
+
+
+run()
